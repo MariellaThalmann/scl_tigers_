40afdf243ba107ebf6ce91e306e810218d81fd58
mari_ella_235 <127301978+MariellaThalmann@users.noreply.github.com>
2024-09-17 13:08:51 +0000

    Colors, fonts, etc.

--- index.js
+++ index.js
@@ -17,6 +17,21 @@ app.get("/impressum", async function (req, res) {
   res.render("impressum", {});
 });
 
+app.get("/views/gallery.handlebars", async function (req, res) {
+  res.render("gallery", {});
+});
+
+app.get("/views/profil.handlebars", async function (req, res) {
+  res.render("profil", {});
+});
+
+app.get("/views/login.handlebars", async function (req, res) {
+  res.render("login", {});
+});
+
+app.get("/views/registration.handlebars", async function (req, res) {
+  res.render("login", {});
+});
 /* Wichtig! Diese Zeilen müssen immer am Schluss der Website stehen! */
 app.listen(3010, () => {
   console.log(`Example app listening at http://localhost:3010`);
